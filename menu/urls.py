a3ab7f823a2185cf1d07d276ed07b736b8c99924
Shokhanasser <shokhanasserprojr.com>
2024-07-25 09:45:55 +0500

    menu adding

--- menu/urls.py
+++ menu/urls.py
@@ -20,7 +20,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     
-    path('accounts/', include('allauth.urls')),
     
     
     
